f2b2ae87dfef03bd82b8e61f9f24d5241b12fc45
Michael Tomlinson <michaeljosephtomlinson@gmail.com>
2019-05-11 10:48:38 -0400

    Admin panel search function

--- BookClub/adminpanel.aspx
+++ BookClub/adminpanel.aspx
@@ -7,7 +7,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>2018 Voting Page</title>
+	<title>Administration Panel</title>
 
 	<link rel="stylesheet" href="assets/demo.css" />
 	<link rel="stylesheet" href="assets/form-basic.css" />
@@ -114,6 +114,7 @@
 
             <div class="form-row">
                 <center><asp:Button id="btnSubmit" runat="server" OnClick="btnSubmit_ClickAsync" Text="Submit" /></center>
+                <center><asp:Button id="btnConfirm" runat="server" OnClick="btnConfirm_Click" Text="Submit" Visible = "false" /></center>
             </div>
 
         </form>
--- BookClub/adminpanel.aspx.cs
+++ BookClub/adminpanel.aspx.cs
@@ -12,6 +12,7 @@ using BookClub.classes.GoogleAPI;
 using System.Xml.Linq;
 using System.Xml;
 using Newtonsoft.Json.Linq;
+using System.Web.UI.HtmlControls;
 
 namespace BookClub
 {
@@ -27,12 +28,62 @@ namespace BookClub
             //Check who is logged in
             string currentUser = HttpContext.Current.User.Identity.Name;
             int userId = conn.getUserID(currentUser);
+
+
+            HtmlTableRow row;
+            HtmlTableCell cell;
+            TextBox txtBox;
+
+
+
+            row = new HtmlTableRow();
+            cell = new HtmlTableCell();
+            txtBox = new TextBox();
+
+            cell.InnerText = "Author";
+            row.Cells.Add(cell);
+
+            txtBox.Width = 200;
+            cell = new HtmlTableCell();
+            cell.Controls.Add(txtBox);
+            row.Cells.Add(cell);
+
+            tableContent.Rows.Add(row);
+
+            row = new HtmlTableRow();
+            cell = new HtmlTableCell();
+            txtBox = new TextBox();
+
+            cell.InnerText = "Title";
+            row.Cells.Add(cell);
+
+            txtBox.Width = 200;
+            cell = new HtmlTableCell();
+            cell.Controls.Add(txtBox);
+            row.Cells.Add(cell);
+
+            tableContent.Rows.Add(row);
+
+            //cell = new HtmlTableCell();
+            //cell.InnerText = "Author";
+            //row.Cells.Add(cell);
+
+            //cell = new HtmlTableCell();
+            //cell.InnerText = "Title";
+            //row.Cells.Add(cell);
+
+
         }
 
         protected void LogoffButton_Click(object sender, EventArgs e)
         {
             FormsAuthentication.SignOut();
             Response.Redirect("login.aspx", true);
+        }
+
+        public void btnConfirm_Click(object sender, EventArgs e)
+        {
+
         }
 
         public async void btnSubmit_ClickAsync(object sender, EventArgs e)
@@ -43,21 +94,103 @@ namespace BookClub
             //client.BaseAddress = new Uri("https://www.googleapis.com/books/v1/");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            string author = "William Gibson";
+            string title = "Neuromancer";
 
-            HttpResponseMessage response = await client.GetAsync(apiURL + "q=neuromancer+inauthor:william+gibson&key=" + apiKEY);
+            foreach (HtmlTableRow row in tableContent.Rows)
+            {
+                foreach (Control ctl in row.Cells[1].Controls.OfType<TextBox>())
+                {
+                    if (row.Cells[0].InnerText == "Author")
+                    {
+                        author = ((TextBox)ctl).Text;
+                    } else if (row.Cells[0].InnerText == "Title")
+                    {
+                        title = ((TextBox)ctl).Text;
+                    }
+                }
+            }
+
+            //HttpResponseMessage response = await client.GetAsync(apiURL + "q=neuromancer+inauthor:william+gibson&key=" + apiKEY);
+            HttpResponseMessage response = await client.GetAsync(apiURL + "q=" + title + "+inauthor:" + author + "&key=" + apiKEY);
             //HttpResponseMessage response = await client.GetAsync("https://www.googleapis.com/books/v1/volumes?q=storm+front+inauthor:butcher&key=AIzaSyAVbJ6aCml5pLODHHIPEexJAd8GJ9Ev6l4");
-
+            googleAPIModel bamresult;
             if (response.IsSuccessStatusCode)
             {
                 string result = response.Content.ReadAsStringAsync().Result;
-
-                //var obj = JsonConvert.DeserializeObject(result);
-                googleAPIModel bamresult = JsonConvert.DeserializeObject<googleAPIModel>(result);
-
-                Console.WriteLine("test");
+                bamresult = JsonConvert.DeserializeObject<googleAPIModel>(result);
+            } else
+            {
+                bamresult = null;
             }
 
+            if (bamresult != null) { draw_table(bamresult); }
+            
+        }
 
+
+        public void draw_table (googleAPIModel gamresult)
+        {
+            HtmlTableRow row;
+            HtmlTableCell cell;
+            RadioButton rb;
+            //TextBox txtBox;
+            int x = 0;
+
+            tableContent.Rows.Clear();
+
+
+            row = new HtmlTableRow();
+            cell = new HtmlTableCell();
+            cell.InnerText = "";
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell();
+            cell.InnerText = "Author";
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell();
+            cell.InnerText = "Title";
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell();
+            cell.InnerText = "Year Published";
+            row.Cells.Add(cell);
+
+            cell = new HtmlTableCell();
+            cell.InnerText = "ISBN";
+            row.Cells.Add(cell);
+
+            tableContent.Rows.Add(row);
+
+            for (x = 0; x <= 5; x++)
+            {
+                row = new HtmlTableRow();
+                cell = new HtmlTableCell();
+                rb = new RadioButton();
+                cell.Controls.Add(rb);
+                row.Cells.Add(cell);
+
+
+                cell = new HtmlTableCell();
+                cell.InnerText = gamresult.items[x].volumeInfo.authors[0];
+                row.Cells.Add(cell);
+
+                cell = new HtmlTableCell();
+                cell.InnerText = gamresult.items[x].volumeInfo.title;
+                row.Cells.Add(cell);
+
+                cell = new HtmlTableCell();
+                cell.InnerText = gamresult.items[x].volumeInfo.publishedDate;
+                row.Cells.Add(cell);
+
+                cell = new HtmlTableCell();
+                cell.InnerText = gamresult.items[x].volumeInfo.industryIdentifier[0].ISBN_identifier;
+                row.Cells.Add(cell);
+
+
+                tableContent.Rows.Add(row);
+            }
         }
     }
 }
